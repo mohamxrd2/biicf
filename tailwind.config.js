a0d668c73107bf7d39fdd14b7d6775a1cedc9141
Saliou <macbookpro@192.168.1.20>
2024-04-12 10:52:00 +0000

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    'node_modules/preline/dist/*.js',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    
+    require('preline/plugin'),
+  ],
+}
+
